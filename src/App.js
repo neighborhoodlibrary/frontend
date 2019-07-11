b1646a64f4b2e50891800c9c6e5feec7006e2e7d
ltrii <lelandtr@gmail.com>
2019-07-11 12:42:35 -0700

    Upgrade dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './views/Landing';
 
 function App() {
   return (
     <div className="App">
-
+      <Landing />
     </div>
   );
 }
---
+++ src/views/Landing.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../components/Header/Header';
+
+export default class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
