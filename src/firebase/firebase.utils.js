56a555d4727137ca40e758efe4dcdd973c95355d
James <ruiz.james12@yahoo.com>
2019-07-11 13:05:04 -0700

    Firebase config

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyA38qCNWOEv6GEmh0EervQ-2MhPJIgLRkw",
+    authDomain: "neighborhood-library-54fa1.firebaseapp.com",
+    databaseURL: "https://neighborhood-library-54fa1.firebaseio.com",
+    projectId: "neighborhood-library-54fa1",
+    storageBucket: "",
+    messagingSenderId: "477537368764",
+    appId: "1:477537368764:web:7d25040fa2254e3e"
+  };
+
+  firebase.initializeApp(config)
+
+  export const auth = firebase.auth()
