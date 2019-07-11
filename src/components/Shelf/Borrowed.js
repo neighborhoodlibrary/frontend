6c7d66f1153a8a15f6f15580a0c58e5030f9989e
ltrii <lelandtr@gmail.com>
2019-07-11 13:14:36 -0700

    Bugfix and changes

---
+++ src/components/Shelf/Borrowed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Borrowed extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
