c3531ec96e6b9c5c1588db54c2749c9fff881a38
James <ruiz.james12@yahoo.com>
2019-07-15 16:11:00 -0700

    User Context set up

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import Auth from "./authentication/Auth";
 import MainContainer from "./views/MainContainer";
 import Landing from "./views/Landing";
+import UserState from './context/user/UserState'
 
 const AuthComponent = Auth(MainContainer)(Landing);
 
 function App() {
   return (
     <div className="App">
-      <AuthComponent />
+      <UserState>
+        <AuthComponent />
+      </UserState>
     </div>
   );
 }
---
+++ src/context/user/UserState.js
@@ -0,0 +1,34 @@
+import UserContext from './userContext'
+import React, { useReducer } from 'react'
+import UserReducer from './userReducer'
+import { GET_USER } from '../types'
+
+const UserState = props => {
+    const initialState = {
+        email: '',
+        displayName: '',
+        uid: '',
+        location: '',
+        books: [],
+        borrowed: [],
+        loaned: []
+    }
+
+    const [state, dispatch] = useReducer(UserReducer, initialState)
+
+    const getUser = () => {
+
+    }
+    
+    return (
+        <UserContext.Provider 
+        value={{
+            user: state,
+            getUser
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserState;
