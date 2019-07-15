47ad28d549b1c774bd5b3ab0b6838a21d79e5f82
ltrii <lelandtr@gmail.com>
2019-07-15 14:58:44 -0700

    Some router work

--- src/views/MyShelf/MyShelf.js
+++ src/views/MyShelf/MyShelf.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+import MainShelf from '../../components/Shelf/MainShelf';
+import Borrowed from '../../components/Shelf/Borrowed';
+import Loaned from '../../components/Shelf/Loaned';
+import Library from '../../components/Shelf/Library';
 
 export default class MyShelf extends Component {
     render() {
         return (
-            <div>
-                This is your shelf
-            </div>
+            <Router>
+                <div>
+                    <Route exact path='/shelf' component={MainShelf} />
+                    <Route exact path='/shelf/borrowed' component={Borrowed} />
+                    <Route exact path='/shelf/loaned' component={Loaned} />
+                    <Route exact path='/shelf/library' component={Library} />
+                </div>
+            </Router>
         )
     }
 }
