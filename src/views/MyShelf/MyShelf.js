8df81a6607d38e0b2ffb2c694b6937641f75bd38
ltrii <lelandtr@gmail.com>
2019-07-16 14:05:55 -0700

    Routing

--- src/views/MyShelf/MyShelf.js
+++ src/views/MyShelf/MyShelf.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 
-import MainShelf from '../../components/Shelf/MainShelf';
-import Borrowed from '../../components/Shelf/Borrowed';
-import Loaned from '../../components/Shelf/Loaned';
-import Library from '../../components/Shelf/Library';
-
 export default class MyShelf extends Component {
     render() {
         return (
-            {/* <Router>
                 <div>
-                    <Route exact path='/shelf' component={MainShelf} />
-                    <Route exact path='/shelf/borrowed' component={Borrowed} />
-                    <Route exact path='/shelf/loaned' component={Loaned} />
-                    <Route exact path='/shelf/library' component={Library} />
+                    
                 </div>
-            </Router> */}
         )
     }
 }
