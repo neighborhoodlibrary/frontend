61fe6b4d7eefc2d154b6c59f749a7107201e6e6f
jeffyjkang <jeffyjkang@gmail.com>
2019-07-17 18:15:14 -0700

    persist data

--- src/context/user/UserState.js
+++ src/context/user/UserState.js
@@ -1,32 +1,41 @@
-import UserContext from './userContext'
-import React, { useReducer } from 'react'
-import UserReducer from './userReducer'
-import { GET_USER, ADD_USER } from '../types'
+import UserContext from "./userContext";
+import React, { useReducer } from "react";
+import UserReducer from "./userReducer";
+import { GET_USER, ADD_USER, SET_LOGIN } from "../types";
 
 const UserState = props => {
-    const initialState = {
-        user: {},
-        loggedIn: false
-    }
+  const initialState = {
+    user: {},
+    loggedIn: false
+  };
 
-    const [state, dispatch] = useReducer(UserReducer, initialState)
+  const [state, dispatch] = useReducer(UserReducer, initialState);
 
-    const addUser = user => {
-        dispatch({
-            type: ADD_USER,
-            payload: user
-        })
-    }
-    
-    return (
-        <UserContext.Provider 
-        value={{
-            userState: state,
-            addUser
-        }}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
+  const addUser = user => {
+    dispatch({
+      type: ADD_USER,
+      payload: user
+    });
+  };
+
+  const setLogin = login => {
+    dispatch({
+      type: SET_LOGIN,
+      payload: login
+    });
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        userState: state,
+        addUser,
+        setLogin
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserState;
--- src/context/user/userReducer.js
+++ src/context/user/userReducer.js
@@ -1,14 +1,20 @@
-import { ADD_USER, GET_USER } from '../types'
+import { ADD_USER, GET_USER, SET_LOGIN } from "../types";
 
 export default (state, action) => {
-    switch(action.type) {
-        case ADD_USER:
-            return {
-                ...state,
-                user: action.payload,
-                loggedIn: true
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loggedIn: true
+      };
+
+    case SET_LOGIN:
+      return {
+        ...state,
+        loggedIn: action.payload
+      };
+    default:
+      return state;
+  }
+};
