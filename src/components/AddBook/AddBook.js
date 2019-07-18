8b6f708ec24d832e10a253affa4492e36fc87150
jeffyjkang <jeffyjkang@gmail.com>
2019-07-18 16:21:39 -0700

    update books to update state

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -33,49 +33,70 @@ export default class AddBook extends Component {
     });
   };
 
-  bookSearch(type = 'title', entry) {
+  // bookSearch(type = 'title', entry) {
+  //   var booksOptions = {
+  //     field: type,
+  //     offset: 0,
+  //     limit: 10,
+  //     type: "books",
+  //     order: "relevance",
+  //     lang: "en"
+  //   };
+
+  //   booksApi.search(entry, booksOptions, function(error, results, apiResponse) {
+  //     const db = firebase.firestore();
+  //     if (!error) {
+  //       console.log(results);
+  //       db.collection('books').doc().set({
+  //         author: results[0].authors[0],
+  //         description: results[0].description
+  //       })
+  //     } else {
+  //       console.log(error);
+  //     }
+  //   });
+  // }
+
+  formSubmit = e => {
+    e.preventDefault();
+
     var booksOptions = {
-      field: type,
+      field: `${this.state.type}`,
       offset: 0,
       limit: 10,
       type: "books",
       order: "relevance",
       lang: "en"
     };
+    let something = results => {
+      this.setState({
+        results
+      });
+    };
 
-    booksApi.search(entry, booksOptions, function(error, results, apiResponse) {
-      const db = firebase.firestore();
-      if (!error) {
-        console.log(results);
-        db.collection('books').doc().set({
-          author: results[0].authors[0],
-          description: results[0].description
-        })
-      } else {
-        console.log(error);
-      }
+    booksApi.search(this.state.entry, booksOptions, function(
+      error,
+      results,
+      apiResponse
+    ) {
+      console.log(results);
+      console.log(something);
+      something(results);
     });
-  }
-
-  formSubmit = e => {
-    e.preventDefault();
-    var resultsarr = this.bookSearch(
-      this.state.type,
-      this.state.entry  );
-    console.log(resultsarr);
   };
 
   render() {
     console.log(this.state.type);
+    console.log(this.state.results);
     return (
       <div>
         <h2>Add a Book!</h2>
         <form onSubmit={this.formSubmit}>
-        <select onChange={this.handleChanges} name="type">
+          <select onChange={this.handleChanges} name="type">
             <option value="title">Title</option>
             <option value="author">Author</option>
             <option value="isbn">ISBN</option>
-        </select>
+          </select>
           <input
             placeholder={this.state.type === null ? "title" : this.state.type}
             onChange={this.handleChanges}
