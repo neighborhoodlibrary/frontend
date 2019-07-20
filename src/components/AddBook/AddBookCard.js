368e4728e805a367bf71612d6da2323b4a3b8346
ltrii <lelandtr@gmail.com>
2019-07-19 20:47:53 -0700

    Add Book updates and reactstrap integration

--- src/components/AddBook/AddBookCard.js
+++ src/components/AddBook/AddBookCard.js
@@ -1,53 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Collapse } from 'reactstrap';
 
 const AddBookCardDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 15px;
-    border: rgba(0,0,0,.5) solid black;
-    border-radius: 2px;
     margin: 15px;
-    max-width: 375px;
-    -webkit-box-shadow: 0px 0px 14px -3px rgba(0,0,0,0.75);
-    -moz-box-shadow: 0px 0px 14px -3px rgba(0,0,0,0.75);
-    box-shadow: 0px 0px 14px -3px rgba(0,0,0,0.75);
-    font-family: 'Merriweather', serif;
-
-    h2{
-        line-height: fit-content;
-        font-size: 1.15em;
-    }
-
-    .authorhold {
-        display: flex;
-
-        h3 {
-            padding: 5px;
-            font-size: 1em;
-        }
-    }
-
-    &:hover {
-        background-color: rgba(0,0,0,.02);
-    }
+    width: 200px;
+    height: 450px;
 `;
 
 export default function AddBookCard(props) {
 
     return (
         <AddBookCardDiv>
-            <h2>
-                {props.book.title}
-            </h2> 
-            <div class="authorhold">
-                
-            </div>
-            <div class="imghold">
-                <img src={props.book.thumbnail} alt="Thumbnail"/>
-            </div>
+            <Card>
+                    <CardBody>
+                        <CardTitle>
+                            {props.book.title}
+                        </CardTitle>
+                    </CardBody>
+                    <CardImg top src={props.book.thumbnail} />
+
+            </Card>
         </AddBookCardDiv>
     )
 }
