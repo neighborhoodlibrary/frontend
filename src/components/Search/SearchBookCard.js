155842a6bb922d1ff65b7c1e6d681e64ac822024
jeffyjkang <jeffyjkang@gmail.com>
2019-07-30 22:32:18 -0700

    update searchbookcard to add uid to books requestedId array and email to owner simultaneously

--- src/components/Search/SearchBookCard.js
+++ src/components/Search/SearchBookCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import firebase from "../../firebase/firebase.utils";
 import { useAlert } from "react-alert";
 import styled from "styled-components";
 import {
@@ -11,6 +12,9 @@ import {
   ModalBody,
   ModalFooter
 } from "reactstrap";
+//
+import Axios from "axios";
+const URL = "https://neighborhoodlibraryback.herokuapp.com/email";
 
 const SearchBookCardDiv = styled.div`
   margin: 15px;
@@ -39,14 +43,60 @@ const SearchBookCardDiv = styled.div`
 `;
 
 const SearchBookCard = props => {
+  const auth = firebase.auth();
+  const user = auth.currentUser;
+  const userDocRef = firebase
+    .firestore()
+    .collection("users")
+    .doc(user.uid);
+  const bookDocRef = firebase
+    .firestore()
+    .collection("books")
+    .doc(props.book.id);
   const [infoModal, setInfoModal] = useState(false);
   const [requestModal, setRequestModal] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
+  const [emailValue, setEmailValue] = useState({
+    to: props.book.ownerEmail,
+    from: userEmail,
+    subject: "Requested Book from Neighborhood Library",
+    text: `User with email: ${userEmail} has requested to borrow one of your books: ${
+      props.book.title
+    }, Please email them back to setup location and time of pickup.`,
+    html: `User with email: ${userEmail} has requested to borrow one of your books: ${
+      props.book.title
+    }, Please email them back to setup location and time of pickup.`
+  });
+
+  useEffect(() => {
+    userDocRef
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          let email = doc.data().email;
+          setUserEmail(email);
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch(error => {
+        console.log("Error getting the document:", error);
+      });
+  }, "");
 
   const toggleInfoModal = () => {
     infoModal ? setInfoModal(false) : setInfoModal(true);
   };
   const toggleRequestModal = () => {
     requestModal ? setRequestModal(false) : setRequestModal(true);
+  };
+  const submitRequest = () => {
+    // const msg = emailValue
+    // Axios.post(URL,msg).then(res=>{
+    //     console.log(res.data)
+    // })
+    //
+    // bookDocRef.update({requestedId: firebase.firestore.FieldValue.arrayUnion(user.uid)})
   };
 
   return (
@@ -102,7 +152,9 @@ const SearchBookCard = props => {
                 user?
               </ModalBody>
               <ModalFooter>
-                <Button color="success">Confirm</Button>
+                <Button onClick={submitRequest} color="success">
+                  Confirm
+                </Button>
                 <Button onClick={toggleRequestModal} color="secondary">
                   Cancel
                 </Button>
