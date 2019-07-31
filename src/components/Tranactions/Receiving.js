0a652a16253eb3c419487525d005d8e5880d49c6
jeffyjkang <jeffyjkang@gmail.com>
2019-07-31 16:25:41 -0700

    add requested and recieving in transactions dir

---
+++ src/components/Tranactions/Receiving.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Receiving = () => {
+  return <div>Receiving Page</div>;
+};
+
+export default Receiving;
---
+++ src/components/Tranactions/Requested.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Requested = () => {
+  return <div>Requested Page</div>;
+};
+
+export default Requested;
