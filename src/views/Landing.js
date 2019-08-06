6857f31971be51fb72dcc22521aae307bd9d6ee1
Anthony Greb <agreb255@gmail.com>
2019-08-06 14:07:18 -0400

    cleanup on landing

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,15 +1,6 @@
 import React, { useContext } from 'react';
 import UserContext from '../context/user/userContext'
 import { Redirect } from 'react-router-dom';
-import styled from 'styled-components';
-// import neighborImg from '../assets/neighborhoodpic.jpg'
-
-// const LandingImg = styled.div`
-//     background: url(${neighborImg});
-//     background-size: cover;
-//     background-position: bottom;
-//     height: 77VH;
-// `
 
 
 
