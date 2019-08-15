8d8b32440364ca6df4fee175e692fda9c063c591
jeffyjkang <jeffyjkang@gmail.com>
2019-08-15 16:54:31 -0700

    fix new user bug on main shelf

--- src/components/Shelf/MainShelf.js
+++ src/components/Shelf/MainShelf.js
@@ -1,18 +1,28 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../../context/user/userContext";
 import { Jumbotron } from "reactstrap";
 
 export default function MainShelf() {
   const userContext = useContext(UserContext);
+  const [user, setUser] = useState({});
+
+  const getUser = () => {
+    setUser(userContext.getUser());
+  };
+  useEffect(() => {
+    getUser();
+  }, {});
+
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">
-          Hello, {userContext.userState.user.displayName}!
-        </h1>
+        <h1 className="display-3">Hello, {user.displayName}!</h1>
         <p className="lead">Welcome to your personal shelf</p>
         <hr className="my-2" />
-        <p>Thanks for visiting. Use the upper navigation to add books to your library to share, or lookup local neighbors to borrow books from! </p>
+        <p>
+          Thanks for visiting. Use the upper navigation to add books to your
+          library to share, or lookup local neighbors to borrow books from!{" "}
+        </p>
       </Jumbotron>
     </div>
   );
