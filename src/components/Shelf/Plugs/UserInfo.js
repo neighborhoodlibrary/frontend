b28f701655959594a0dd0812c9b7d2c0ec5f676e
jeffyjkang <jeffyjkang@gmail.com>
2019-08-23 16:41:44 -0700

    main shelf brings in personalBookCount

--- src/components/Shelf/Plugs/UserInfo.js
+++ src/components/Shelf/Plugs/UserInfo.js
@@ -1,34 +1,28 @@
-import React, { useContext, useEffect, useState } from 'react';
-import UserContext from '../../../context/user/userContext';
-import styled from 'styled-components';
+import React, { useContext, useEffect, useState } from "react";
+import UserContext from "../../../context/user/userContext";
+import styled from "styled-components";
 
 const UserInfoDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: #6CC444;
-    color: white;
-    padding: 5px;
-    font-size: 0.9em;
-    font-family: 'Merriweather Sans';
-    border-radius: 2px;
-`
+  display: flex;
+  flex-direction: column;
+  background-color: #6cc444;
+  color: white;
+  padding: 5px;
+  font-size: 0.9em;
+  font-family: "Merriweather Sans";
+  border-radius: 2px;
+`;
 
 export default function UserInfo() {
-    const userContext = useContext(UserContext);
+  const userContext = useContext(UserContext);
 
-    var curUser = userContext.userState.user;
+  var curUser = userContext.userState.user;
 
-    return (
-        <UserInfoDiv>
-            <div>
-                {curUser.displayName}
-            </div>
-            <div>
-                {curUser.email}
-            </div>
-            <div>
-              Books in library:  {curUser.books ? curUser.books.length : null }
-            </div>
-        </UserInfoDiv>
-    )
+  return (
+    <UserInfoDiv>
+      <div>{curUser.displayName}</div>
+      <div>{curUser.email}</div>
+      <div>Books in library: {curUser.personalBookCount}</div>
+    </UserInfoDiv>
+  );
 }
