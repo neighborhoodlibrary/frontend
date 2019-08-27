e6aac2bf143e7a195fa480d4b39f60b8297122fd
jeffyjkang <jeffyjkang@gmail.com>
2019-08-27 08:39:27 -0700

    remove unecessary dependencies from loanedbook

--- src/components/Shelf/LoanedBook.js
+++ src/components/Shelf/LoanedBook.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import firebase from "../../firebase/firebase.utils";
-import { useAlert } from "react-alert";
+// import firebase from "../../firebase/firebase.utils";
+// import { useAlert } from "react-alert";
 import {
   Card,
   CardHeader,
