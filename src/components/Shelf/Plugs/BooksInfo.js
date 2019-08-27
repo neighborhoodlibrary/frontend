99b3ecac10900c1d324401654e65388ce1c4a348
jeffyjkang <jeffyjkang@gmail.com>
2019-08-26 19:12:40 -0700

    very basic setup of homepage, requested, returning, loan and borrowing

--- src/components/Shelf/Plugs/BooksInfo.js
+++ src/components/Shelf/Plugs/BooksInfo.js
@@ -7,9 +7,19 @@ import styled from "styled-components";
 
 const BorrowedDiv = styled.div`
   display: flex;
+  margin: 1rem;
 `;
 const LoanedDiv = styled.div`
   display: flex;
+  margin: 1rem;
+`;
+const RequestedDiv = styled.div`
+  display: flex;
+  margin: 1rem;
+`;
+const ReturningDiv = styled.div`
+  display: flex;
+  margin: 1rem;
 `;
 
 const BooksInfo = () => {
@@ -20,6 +30,8 @@ const BooksInfo = () => {
   const docRef = firebase.firestore().collection("books");
   const [borrowedBooks, setBorrowedBooks] = useState(null);
   const [loanedBooks, setLoanedBooks] = useState(null);
+  const [requestedBooks, setRequestedBooks] = useState(null);
+  const [returningBooks, setReturningBooks] = useState(null);
 
   useEffect(() => {
     getBooks();
@@ -28,6 +40,8 @@ const BooksInfo = () => {
 
   const getBooks = () => {
     const tempLoanedArr = [];
+    const tempRequestedArr = [];
+    const tempReturningArr = [];
     docRef
       .where("ownerId", "==", curUid)
       .get()
@@ -36,6 +50,12 @@ const BooksInfo = () => {
           if (doc.data().checkedOut === true) {
             tempLoanedArr.push(doc.data());
           }
+          if (doc.data().requestedId.length > 0) {
+            tempRequestedArr.push(doc.data());
+          }
+          if (doc.data().transitionUser === curUid) {
+            tempReturningArr.push(doc.data());
+          }
         });
       })
       .then(() => {
@@ -43,6 +63,16 @@ const BooksInfo = () => {
           setLoanedBooks(tempLoanedArr);
         } else {
           setLoanedBooks(null);
+        }
+        if (tempRequestedArr.length > 0) {
+          setRequestedBooks(tempRequestedArr);
+        } else {
+          setRequestedBooks(null);
+        }
+        if (tempReturningArr.length > 0) {
+          setReturningBooks(tempReturningArr);
+        } else {
+          setReturningBooks(null);
         }
       });
   };
@@ -68,11 +98,45 @@ const BooksInfo = () => {
         console.log("Error getting the documents:", error);
       });
   };
-  console.log(borrowedBooks);
+
   return (
     <div>
+      <RequestedDiv>
+        {requestedBooks === null ? (
+          <div>You have 0 books that have been requested</div>
+        ) : (
+          <div>
+            <div>
+              You have {requestedBooks.length} books that are being requested
+            </div>
+            {requestedBooks.map(requestedBook => (
+              <div key={Math.random()}>
+                <div>Title: {requestedBook.title}</div>
+                <div>Requested by: {requestedBook.requestedId.join(" , ")}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </RequestedDiv>
+      <ReturningDiv>
+        {returningBooks === null ? (
+          <div>You have 0 books being returned</div>
+        ) : (
+          <div>
+            <div>
+              You have {returningBooks.length} books that are being returned
+            </div>
+            {returningBooks.map(returningBook => (
+              <div key={Math.random()}>
+                <div>Title: {returningBook.title}</div>
+                <div>Returning from: {returningBook.borrowerId}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ReturningDiv>
       <LoanedDiv>
-        You are currently loaning out:{" "}
+        You are currently loaning out:
         {loanedBooks === null ? (
           <div>0 books</div>
         ) : (
