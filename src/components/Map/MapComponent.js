d1442fb0f6911e8b76cfc4626830b30d16febb1e
ltrii <lelandtr@gmail.com>
2019-09-09 22:21:19 -0700

    map cleanup

--- src/components/Map/MapComponent.js
+++ src/components/Map/MapComponent.js
@@ -27,13 +27,15 @@ const mapApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const ContainerDiv = styled.div`
   width: 100%;
+  margin: auto;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
+  align-items: center;
+  box-sizing: border-box;
 
   @media (max-width: 800px) {
     flex-direction: column;
     justify-content: center;
-    align-items: center;
   }
 `;
 
@@ -42,10 +44,14 @@ const MapContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  margin: 10px;
-  height: 50vh;
+  height: 500px;
   border-radius: 2px;
 `;
+
+const CardHolder = styled.div`
+  box-sizing: border-box;
+  margin: 10px;
+`
 
 const MapComponent = props => {
   const auth = firebase.auth();
@@ -156,56 +162,56 @@ const MapComponent = props => {
           </Map>
         </MapContainer>
       </Col>
-      <Col xs="12" md="4">
-        <Card>
-          <CardHeader tag="h3">Set-Up Personal Library Location</CardHeader>
-          <CardBody>
-            <CardTitle>
-              <b>Please click on the map to create a marker</b>
-            </CardTitle>
-            <CardText>
-              This will set the coordinates to initialize your personal library
-              location
-            </CardText>
-            <CardTitle>
-              <b>
-                Or if you would like, input an address and click set address
-              </b>
-            </CardTitle>
-            <div>
-              <InputGroup>
-                <Input
-                  name="addressValue"
-                  value={addressValue}
-                  onChange={handleChanges}
-                  type="text"
-                  placeholder="input address"
-                />
-                <InputGroupAddon addonType="append">
-                  <Button onClick={submitAddress}>Set Address</Button>
-                </InputGroupAddon>
-              </InputGroup>
-            </div>
-            <CardText>
-              Then click submit location to finalize the location of your
-              personal library
-            </CardText>
-          </CardBody>
-          <CardFooter>
-            <Button onClick={toggleLocationModal}>Submit Location</Button>
-          </CardFooter>
-        </Card>
-        <Modal isOpen={locationModal} toggle={toggleLocationModal} centered>
-          <ModalHeader>Set Location</ModalHeader>
-          <ModalBody>
-            Are you sure you want to set your personal library to this location?
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={submitCoordinates}>Confirm</Button>
-            <Button onClick={toggleLocationModal}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </Col>
+        <CardHolder>
+          <Card>
+            <CardHeader tag="h3">Set-Up Personal Library Location</CardHeader>
+            <CardBody>
+              <CardTitle>
+                <b>Please click on the map to create a marker</b>
+              </CardTitle>
+              <CardText>
+                This will set the coordinates to initialize your personal library
+                location
+              </CardText>
+              <CardTitle>
+                <b>
+                  Or if you would like, input an address and click set address
+                </b>
+              </CardTitle>
+              <div>
+                <InputGroup>
+                  <Input
+                    name="addressValue"
+                    value={addressValue}
+                    onChange={handleChanges}
+                    type="text"
+                    placeholder="input address"
+                  />
+                  <InputGroupAddon addonType="append">
+                    <Button onClick={submitAddress}>Set Address</Button>
+                  </InputGroupAddon>
+                </InputGroup>
+              </div>
+              <CardText>
+                Then click submit location to finalize the location of your
+                personal library
+              </CardText>
+            </CardBody>
+            <CardFooter>
+              <Button onClick={toggleLocationModal}>Submit Location</Button>
+            </CardFooter>
+          </Card>
+          <Modal isOpen={locationModal} toggle={toggleLocationModal} centered>
+            <ModalHeader>Set Location</ModalHeader>
+            <ModalBody>
+              Are you sure you want to set your personal library to this location?
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={submitCoordinates}>Confirm</Button>
+              <Button onClick={toggleLocationModal}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </CardHolder>
     </ContainerDiv>
   );
 };
